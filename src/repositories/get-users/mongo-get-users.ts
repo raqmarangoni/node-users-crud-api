e27c88ec074fb761bf5de25abfa2016642be5e3d
Raquel Marangoni <rmarangonisan@gmail.com>
2024-12-11 23:38:28 -0300

    feat: ensure GetUsersMongoRepository get users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,13 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/users";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return await Promise.resolve([
-      {
-        firstName: "Raquel",
-        lastName: "Marangoni",
-        email: "raquel@mail.com",
-        password: "123",
-      },
-    ]);
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+    return users.map((user) => ({ ...user, id: user._id.toHexString() }));
   }
 }
