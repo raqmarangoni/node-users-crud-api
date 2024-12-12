158635824071f5f2cd62c405f0aa44edbf40a853
Raquel Marangoni <rmarangonisan@gmail.com>
2024-12-11 21:53:05 -0300

    feat: add express initial server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import express from 'express'
+import {config} from 'dotenv'
+
+config()
+const app = express()
+const port = process.env.PORT || 8000
+
+app.get('/', (_, res) => {
+    res.json('Hello World!')
+})
+app.listen(port, () => console.log(`listening on port ${port}}`))
